ea48503f931450015625c8b1f7b638d5d9c39693
John Potts <john@arlingtonchurch.com>
2017-09-26 05:56:13 -0400

    Add controllers

---
+++ MVCMusicStore/Migrations/201709260951589_email.cs
@@ -0,0 +1,18 @@
+namespace MVCMusicStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class email : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Reviews", "ReviewerEmail", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Reviews", "ReviewerEmail", c => c.String());
+        }
+    }
+}
